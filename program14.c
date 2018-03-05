93c8cbf95d26a966ebdce073fbc9df1e43fc2e76
Muthukumarll <35914910+Muthukumarll@users.noreply.github.com>
2018-03-05 13:43:49 +0530

    program14.c

---
+++ program14.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+ int n, i, count = 0;
+clrscr();
+ scanf("%d",&n);
+ for(i=0;i<=n/2;i++)
+ {
+ if(n % i == 0 )
+  {
+   count = 1;
+   break;
+  }
+ }
+ if(count == 0)
+  printf("no");
+ else
+  printf("yes");
+ getch();
+ return 0;
+}
